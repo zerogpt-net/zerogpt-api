ed591f26838ee82f10ead87cc2606f74ceb2cb09
Mohamed AIT LARAYEK <mohamedaitlarayek@gmail.com>
2023-06-04 00:36:54 +0100

    init

---
+++ zerogpt.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+const options = {
+  method: 'POST',
+  url: 'https://zerogpt.p.rapidapi.com/api/v1/detectText',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '38e42b9145msh1525089f91a1970p13bb82jsne051cefd2d64',
+    'X-RapidAPI-Host': 'zerogpt.p.rapidapi.com'
+  },
+  data: {
+    input_text: 'C++ is a high-level, general-purpose programming language...'
+  }
+};
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
+}
---
+++ zerogpt.php
@@ -0,0 +1,33 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://zerogpt.p.rapidapi.com/api/v1/detectText",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode([
+    'input_text' => 'C++ is a high-level, general-purpose programming language...'
+  ]),
+  CURLOPT_HTTPHEADER => [
+    "X-RapidAPI-Host: zerogpt.p.rapidapi.com",
+    "X-RapidAPI-Key: 38e42b9145msh1525089f91a1970p13bb82jsne051cefd2d64",
+    "content-type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+?>
